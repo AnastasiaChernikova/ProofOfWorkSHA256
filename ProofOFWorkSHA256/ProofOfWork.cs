db993fbde04f7d1baaa4d5e198eba66aa9af44dc
AnastasiaChernikova <chernikovanastia28@gmail.com>
2021-04-05 20:21:32 +0300

    Add clases for storage and proof of work general flow

---
+++ ProofOFWorkSHA256/ProofOfWork.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProofOFWorkSHA256
+{
+    public class ProofOfWork
+    {
+        public static StorageOfData StartProofOfWork(string testData, int startingZeros)
+        {
+            var resultData = new StorageOfData();
+
+            // startingZeros - number of starting zero bits resulting SHA256 block hash should contain
+            var expectedResult = new string('0', startingZeros);
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            while(true)
+            {
+                if ((!resultData.HashData.StartsWith(expectedResult)))
+                {
+                    resultData.NumberOfIterations++;
+                    resultData.Data = testData + RandomStringGenerator.GenerateByLen(10);
+                    resultData.HashData = HashOperations.GenerateSha256String(resultData.Data);
+                }
+                else
+                {
+                    break;
+                }
+            }
+            resultData.Time = stopwatch.Elapsed.TotalSeconds;
+            return resultData;
+        }
+    }
+}
---
+++ ProofOFWorkSHA256/StorageOfData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProofOFWorkSHA256
+{
+    public class StorageOfData
+    {
+        // string that we use as data
+        public string Data { get; set; }
+     
+        public double Time { get; set; }
+        // hash of data
+        public string HashData { get; set; }
+
+        public int NumberOfIterations { get; set; }
+    }
+}
